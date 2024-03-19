4020aac2f40298255d9d0e3fb72c9e137456eb6b
Chen Peng <chen56@msn.com>
2024-03-20 00:37:47 +0800

    /*tailwind 会破坏原有的markdown样式，所以要包一层:         * 1. pnpm install -D @tailwindcss/typography         * 2. tailwind.config.mjs 增加 plugins[require('@tailwindcss/typography')]         * 3. 用一个特殊layout包住所有markdown: <div class="prose prose-stone lg:prose"><slot/></div>         * */

--- app/packages/app-astro/tailwind.config.mjs
+++ app/packages/app-astro/tailwind.config.mjs
@@ -4,5 +4,13 @@ export default {
     theme: {
         extend: {},
     },
-    plugins: [require("daisyui")],
+    plugins: [
+        require("daisyui"),
+        /*tailwind 会破坏原有的markdown样式，所以要包一层:
+        * 1. pnpm install -D @tailwindcss/typography
+        * 2. tailwind.config.mjs 增加 plugins[require('@tailwindcss/typography')]
+        * 3. 用一个特殊layout包住所有markdown: <div class="prose prose-stone lg:prose"><slot/></div>
+        * */
+        require('@tailwindcss/typography')
+    ],
 }
