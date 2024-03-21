6219c99d756feaf2f970dc3a5da0411e9e96e7f8
Chen Peng <chen56@msn.com>
2024-03-21 20:23:19 +0800

    mui 就是全乎，弄了个框架ok

--- app/packages/app-nextjs/src/app/page.tsx
+++ app/packages/app-nextjs/src/app/page.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import Link from 'next/link';
 
 export default function ButtonUsage() {
-    return <Button variant="contained">Hello world</Button>;
+    return <div>
+        <Link href={"/workspace"}>/workspace</Link>
+        <br/>
+        <Link href={"/"}>/</Link>
+        <br/>
+
+        <Button variant="contained">Hello world </Button>
+    </div>
+        ;
 }
