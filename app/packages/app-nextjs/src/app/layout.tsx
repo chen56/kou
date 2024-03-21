3888d06d5387a1aedfcc665ec1de7614194d076a
Chen Peng <chen56@msn.com>
2024-03-21 17:57:22 +0800

    nextjs集成radix-ui/themes 组件太少了，这玩意能用吗

--- app/packages/app-nextjs/src/app/layout.tsx
+++ app/packages/app-nextjs/src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
+import '@radix-ui/themes/styles.css';
+import {Theme} from '@radix-ui/themes';
+
 import "./globals.css";
+import React from "react";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -16,7 +20,11 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+        <Theme>
+            {children}
+        </Theme>
+        </body>
         </html>
     );
 }
