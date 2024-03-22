08d09da04cacaab4ea5d0ecbb9cb094d1e48ae82
Chen Peng <chen56@msn.com>
2024-03-22 19:02:05 +0800

    布局

--- app/packages/app-nextjs/src/app/page.tsx
+++ app/packages/app-nextjs/src/app/page.tsx
@@ -1,17 +1,7 @@
-import * as React from 'react';
-import Link from 'next/link';
+'use server'
 
-export default function ButtonUsage() {
-    return <div className="">
-        <Link href={"/workspace"}>/workspace</Link>
-        <br/>
-        <Link href={"/"}>/</Link>
-        <br/>
-        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+import {redirect} from 'next/navigation'
 
-        <button className="btn btn-primary">One</button>
-        <button className="btn btn-secondary">Two</button>
-        <button className="btn btn-accent btn-outline">Three</button>
-    </div>
-        ;
+export default async function ButtonUsage() {
+    redirect(`/workspace`) // Navigate to the new post page
 }
