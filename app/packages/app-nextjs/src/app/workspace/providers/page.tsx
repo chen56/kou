9578d88fa0d515b1a2142821173732d2a4e73c37
Chen Peng <chen56@msn.com>
2024-03-22 19:02:13 +0800

    布局

---
+++ app/packages/app-nextjs/src/app/workspace/providers/page.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import Link from 'next/link';
+
+export default function ButtonUsage() {
+    return <div className="">
+        <Link href={"/workspace"}>/workspace</Link>
+        <br/>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+        <Link href={"/"}>/</Link>
+        <br/>
+        <button className="btn btn-primary">Buttosssssssssssssssssssssn</button>
+
+        <button className="btn btn-primary">One</button>
+        <button className="btn btn-secondary">Two</button>
+        <button className="btn btn-accent btn-outline">Three</button>
+    </div>
+        ;
+}
